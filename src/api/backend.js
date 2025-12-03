7dc09d31290c624c340767db0d3e28d7ee4bba2b
Aditya Kumar <aditya32193213@gmail.com>
2025-12-04 04:59:11 +0530

    updated commits

--- src/api/backend.js
+++ src/api/backend.js
@@ -1,34 +1,11 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "https://ai-orchestrator-backend-f0k6.onrender.com",
-// });
-
-// export default API;
-
-
-
-// //Back-end API for testing
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "http://localhost:5000",
-// });
-
-// export default API;
-
-
-
-
 import axios from "axios";
 
-const BASE = "http://localhost:10000"|| process.env.REACT_APP_API_URL;
+const BASE = process.env.REACT_APP_API_URL || "http://localhost:10000" ;
 
 const API = axios.create({
   baseURL: BASE,
   timeout: 60000,
   headers: {
-    // default headers — content-type will be set per request (form-data vs json)
   }
 });
 
